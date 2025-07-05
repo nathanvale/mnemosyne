b2d132ec3db43ab55abfad42053f838cf898d8f1
Nathan Vale <hi@nathanvale.com>
2025-07-05 15:07:16 +1000

    feat: adds Prisma integration and CSV import functionality
    Integrates Prisma with SQLite for database operations, including schema setup, migrations, and client generation.
    
    Introduces a script for importing messages from CSV files, with support for handling duplicates, preview mode, and detailed error handling.
    
    Updates dependencies and scripts for database management and file operations. Adds corresponding unit tests to ensure reliability.

---
+++ prisma/migrations/20250704085329_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "timestamp" DATETIME NOT NULL,
+    "sender" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Link" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "url" TEXT NOT NULL,
+    "messageId" INTEGER NOT NULL,
+    CONSTRAINT "Link_messageId_fkey" FOREIGN KEY ("messageId") REFERENCES "Message" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Asset" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "filename" TEXT NOT NULL,
+    "messageId" INTEGER NOT NULL,
+    CONSTRAINT "Asset_messageId_fkey" FOREIGN KEY ("messageId") REFERENCES "Message" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Message_hash_key" ON "Message"("hash");
